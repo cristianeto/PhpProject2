735120ee27615567d73e3f46f409695c3fbc76b0
cristianeto <cristiang_guamanb@hotmail.com>
2014-11-28 11:37:11 -0500

    phpPrj Vie28Nov
    Editado el index

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        
+        <?php
+        echo "<h1>Bienvenido</h1>";
+        for($i=0; $i<5; $i++)
+        echo "Ola ke ase";
+        
+        
+        echo	"<h1>Tabla de multiplicar del 7</h1>";
+	echo "<h2>Tabla del 7</h2>";
+
+// Bucle de 1 a 10
+for($i=1; $i<11; $i++)
+echo "7 x $i = ".(7*$i)."<br>\n";
+        
+        ?>
+    </body>
+</html>
